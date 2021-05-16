a0a8ff7311ede814124f131d060d1a010bb4c0f7
danvil97 <danvil97@yandex.ru>
2021-05-16 13:32:05 +0300

    [atch]: Hide api keys to .env file

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,12 +2,21 @@
 ! Weather API
 ? https://www.weatherapi.com/
 */
-const WEATHERAPI_KEY = 'd1bd5466856d4411ada214841213004';
 const WEATHERAPI_BASEURL = 'https://api.weatherapi.com/v1';
 
 export const WeatherAPI = {
   getCurrentWeather: (cityName) =>
-    `${WEATHERAPI_BASEURL}/current.json?key=${WEATHERAPI_KEY}&q=${cityName}`,
+    `${WEATHERAPI_BASEURL}/current.json?key=${process.env.REACT_APP_WEATHER_KEY}&q=${cityName}`,
+};
+
+/* 
+! Currency API
+? https://fixer.io/
+*/
+const CURRENCY_BASEURL = 'http://data.fixer.io/api';
+
+export const CurrencyAPI = {
+  getLatest: () => `${CURRENCY_BASEURL}/latest?access_key=${process.env.REACT_APP_CURRENCY_KEY}`,
 };
 
 export const placeholder = {};
